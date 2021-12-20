cc84155555c8564113ef27f936f4d396a21e812c
Loïc Maurin <loic.maurin@student.ecp.fr>
2021-12-20 12:04:00 +0100

    AppBar added to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Drawer, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import Home from './components/Home';
 import Charts from './components/Charts';
 import Tables from './components/Tables';
 import Map from './components/Map';
-import Layout from './components/Layout';
 
 export default function App() {
   return (
@@ -19,3 +21,54 @@ export default function App() {
     </div>
   );
 }
+
+function Layout() {
+  const open = true;
+
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Sample PWA
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant='permanent' open={open}>
+
+      </Drawer>
+          <div>
+          <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/charts">Charts</Link>
+          </li>
+          <li>
+            <Link to="/tables">Table</Link>
+          </li>
+          <li>
+            <Link to="/map">Map</Link>
+          </li>
+        </ul>
+        </nav>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
