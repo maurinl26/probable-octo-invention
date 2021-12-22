11341bb6ea7fbe34936f32b491cb9c902d494423
Loïc Maurin <loic.maurin@student.ecp.fr>
2021-12-22 17:06:19 +0100

    Theme added to Home

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,47 @@
 import * as React from 'react';
-import { Box, Typography } from "@mui/material";
+import { Box, CardHeader, Typography, useTheme, Card } from "@mui/material";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 import { styled } from '@mui/system';
+
+import L from 'leaflet/dist/leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Search } from '@mui/icons-material';
 
 export default function Map() {
+    const theme = useTheme();
     const position = [51.505, -0.09];
+    const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
 
     const SMapContainer = styled(MapContainer)({
         maxHeight: '600px',
         position: 'relative',
+        backgroundColor: theme.palette.primary.light,
+    });
+
+    const SBox = styled(Box)({
+        px: 2,
     });
 
     return (
-        <Box sx={{boxSizing:'border-box'}}>
-            <Typography> Voici la carte </Typography>
+        <SBox>
+            <Typography variant='h5'> Voici la carte </Typography>
+            <Card>
+                <CardHeader>
+                    <Typography variant='h5'>Carte du Monde</Typography>
+                </CardHeader>
             <SMapContainer center={position} zoom={13} scrollWheelZoom={false} sx={{position:'relative'}}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
-                    </Marker>
+                <Marker position={position} icon={icon} />
             </SMapContainer>
-        </Box>    
+
+            </Card>
+            
+        </SBox>    
     );
 }
   
