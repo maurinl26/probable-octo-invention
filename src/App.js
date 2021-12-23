df9781ee54ef1703982113950bb01f1d8cba607f
Loïc Maurin <loic.maurin@student.ecp.fr>
2021-12-23 16:38:33 +0100

    Microsoft authentication added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
@@ -6,19 +7,29 @@ import Home from './components/Home';
 import Charts from './components/Charts';
 import Tables from './components/Tables';
 import Map from './components/Map';
+import PageLayout from './components/PageLayout';
 
 export default function App() {
-  
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="home" element={<Home />}></Route>
-          <Route path="charts" element={<Charts />}></Route>
-          <Route path="tables" element={<Tables />}></Route>
-          <Route path="map" element={<Map />}></Route>
-        </Route>
-      </Routes>
+      <PageLayout>
+        <AuthenticatedTemplate>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="home" element={<Home />}></Route>
+              <Route path="charts" element={<Charts />}></Route>
+              <Route path="tables" element={<Tables />}></Route>
+              <Route path="map" element={<Map />}></Route>
+            </Route>
+          </Routes>
+        </AuthenticatedTemplate>
+
+        <UnauthenticatedTemplate>
+          <h5>Vous n'êtes pas authentifié</h5>
+        </UnauthenticatedTemplate>
+
+      </PageLayout>
     </div>
   );
 }
